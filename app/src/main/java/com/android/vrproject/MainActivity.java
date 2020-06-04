5862a6e0eae46e0cc08ae44c7a59f962a16fed80
luwalker <luwalker@uw.edu>
2020-06-04 23:37:21 +0200

    version that works, however without scale or rotation

--- app/src/main/java/com/android/vrproject/MainActivity.java
+++ app/src/main/java/com/android/vrproject/MainActivity.java
@@ -15,8 +15,13 @@ import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.HashMap;
@@ -33,11 +38,17 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseManager firebaseManager;
 
     private ModelRenderable modelRenderable;
-    private AnchorNode anchorNode;
+    private ModelRenderable anchorRedRenderable;
+    private ModelRenderable anchorGreenRenderable;
+
+    private AnchorNode redAnchorNode;
+    private AnchorNode greenAnchorNode;
+    private Anchor anchor;
+    private Anchor hostingAnchor;
     private Map<Integer, TransformableNode> transformableNodeMap;
     private Scene arScene;
     private SceneData sceneData;
-    private int shortCode;
+    private int shortCode = 0;
 
     private Button clearButton;
     private Button resolveButton;
@@ -55,22 +66,48 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
 
         arScene = fragment.getArSceneView().getScene();
-        arScene.addOnUpdateListener(frameTime -> cloudAnchorManager.onUpdate());
-
-        transformableNodeMap = new HashMap<>();
+        arScene.addOnUpdateListener(frameTime -> {
+            cloudAnchorManager.onUpdate();
+            if (anchor != null) Log.e("ANCHOR POSITION", anchor.getPose().toString());
+        });
 
         ModelRenderable.builder()
                 .setSource(this, R.raw.andy)
                 .build()
                 .thenAccept(modelRenderable -> this.modelRenderable = modelRenderable);
 
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+                .thenAccept(
+                        material -> {
+                            anchorRedRenderable =
+                                    ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.0f, 0.0f), material); });
+
+
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.GREEN))
+                .thenAccept(
+                        material -> {
+                            anchorGreenRenderable =
+                                    ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.0f, 0.0f), material); });
+
+
 
         // listen for tap --> set AnchorNode and put Andy on top of it
         fragment.setOnTapArPlaneListener(
                 (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
-                    createNewCloudAnchor(hitresult);
+                    if (anchor != null) {
+                        // either new anchor already created or old one already resolved
+                        addNewObject();
+                    }
+                    else {
+                        createNewCloudAnchor(hitresult);
+                    }
                 }
         );
+
+        sceneData = new SceneData();
+        sceneData.setNodeDataMap(new HashMap<>());
+
+        transformableNodeMap = new HashMap<>();
 
         // initialize buttons
         View buttonPanel = findViewById(R.id.buttonPanel);
@@ -82,69 +119,132 @@ public class MainActivity extends AppCompatActivity {
         resolveButton.setOnClickListener(v -> onResolveButtonPressed());
 
         infoText = buttonPanel.findViewById(R.id.infotext);
-
     }
 
+
+
+
+
+
+    /*
+    OBJECT FUNCTIONS
+     */
+
+
+
+
+
     private void createNewCloudAnchor(HitResult hitResult) {
-
-        infoText.setText("Plane tapped");
-
-        // renderable not yet loaded
-        if (modelRenderable == null){
-            return;
-        }
-
-        if (anchorNode != null) {
-            // Do nothing if there was already an anchor in the Scene.
-
-            int index = sceneData.addNew();
-            TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
-            transformableNodeMap.put(index, node);
-
-            initObject(index);
-            updateObjects();
-
-            return;
-        }
-
-        sceneData = new SceneData();
-        sceneData.setNodeDataMap(new HashMap<>());
-
         // set new AnchorNode into the scene
-        Anchor anchor = hitResult.createAnchor();
-        anchorNode = new AnchorNode(anchor);
-        anchorNode.setParent(fragment.getArSceneView().getScene());
+        hostingAnchor = hitResult.createAnchor();
+        redAnchorNode = new AnchorNode(hostingAnchor);
+        redAnchorNode.setParent(fragment.getArSceneView().getScene());
+        Node node = new Node();
+        node.setParent(redAnchorNode);
+        node.setRenderable(anchorRedRenderable);
 
-        // add new node and nodeData to Scene
+        cloudAnchorManager.hostCloudAnchor(
+                fragment.getArSceneView().getSession(), hostingAnchor, this::onHostedAnchorAvailable);
+    }
+
+    public void addNewObject() {
+        if (shortCode == 0) {
+            // not hosted yet --> wait
+            return;
+        }
+
+        // add new object to this anchor
         int index = sceneData.addNew();
         TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
         transformableNodeMap.put(index, node);
 
         initObject(index);
+
+        if (anchor != null) {
+            firebaseManager.updateSceneData(getShortCode(), sceneData);
+            //updateMessages(node);
+        }
+    }
+
+
+    public void setSceneData(SceneData sceneData){
+        this.sceneData = sceneData;
+
         updateObjects();
-
-        cloudAnchorManager.hostCloudAnchor(
-                fragment.getArSceneView().getSession(), anchor, this::onHostedAnchorAvailable);
     }
 
-    public int getShortCode(){
-        return  shortCode;
+    public void initObject(int index){
+        // put Andy on the anchor
+        TransformableNode node = transformableNodeMap.get(index);
+        NodeData nodeData = sceneData.getNodeData(index);
+
+        float[] anchorPosition = anchor.getPose().getTranslation();
+
+        //add new anchorNode right on top of original anchor --> can then be moved
+        AnchorNode anchorNode = new AnchorNode();
+        //anchorNode.setWorldPosition(new Vector3(anchorPosition[0], anchorPosition[1], anchorPosition[2]));
+        anchorNode.setParent(greenAnchorNode);//fragment.getArSceneView().getScene());
+        anchorNode.setLocalPosition(new Vector3(0,0,0));
+
+        node.setParent(anchorNode);
+        node.setRenderable(modelRenderable);
+        node.select();
+
+        node.getScaleController().setMinScale(0.2f);
+        node.getScaleController().setMaxScale(5f);
+
+        node.setOnTouchListener((hitTestResult, event) -> {
+            nodeData.setPosition(node.getParent().getLocalPosition());
+            //nodeData.setScale(node.getParent().getWorldScale());
+            //nodeData.setRotation(node.getParent().getWorldRotation());
+            sceneData.setNodeData(index, nodeData);
+
+            if (event.getAction() == MotionEvent.ACTION_UP ) {
+                if (anchor != null) {
+                    firebaseManager.updateSceneData(getShortCode(), sceneData);
+                    //updateMessages(node);
+                }
+            }
+            return true;
+        });
     }
 
-    public void setShortCode(int shortCode){
-        this.shortCode = shortCode;
+
+    public void updateObjects() {
+        for (int index : transformableNodeMap.keySet()) {
+            Log.e("INDEX", ((Integer)index).toString());
+            NodeData nodeData = sceneData.getNodeData(index);
+            TransformableNode node = transformableNodeMap.get(index);
+
+            node.getParent().setLocalPosition(nodeData.getPosition());
+            //node.getParent().setWorldScale(nodeData.getScale());
+            //node.getParent().setWorldRotation(nodeData.getRotation());
+        }
+
+
+/*
+        greenAnchorNode = new AnchorNode(anchor);
+        greenAnchorNode.setParent(fragment.getArSceneView().getScene());
+
+
+        Node node = new Node();
+        node.setParent(greenAnchorNode);
+        node.setRenderable(anchorGreenRenderable);
+ */
+
     }
+
+
+
+
+
+    /*
+    LISTENER FUNCTIONS
+     */
+
 
     private void onClearButtonPressed() {
-        infoText.setText("cleared");
-
-        // clear pending listeners
-        cloudAnchorManager.clearListeners();
-
-        shortCode = 0;
-
-        // Clear the anchor from the scene.
-        updateAnchor(null);
+        clear();
     }
 
     // show resolve dialog, ask for CloudAnchor ID, then call onShortCodeEntered
@@ -154,72 +254,55 @@ public class MainActivity extends AppCompatActivity {
         dialog.show(fragment.getFragmentManager(), "Resolve");
     }
 
-    private void updateAnchor(Anchor anchor)
-    {
-        if (anchor == null) {
-            // reset pressed
-            if (anchorNode != null) {
-                arScene.removeChild(anchorNode);
-                anchorNode = null;
-            }
-
-            return;
-        }
-
-        anchorNode = new AnchorNode(anchor);
-        anchorNode.setParent(fragment.getArSceneView().getScene());
-
-        // sceneData is up todate, need to update TransformationNodeMap
-        transformableNodeMap = sceneData.getTransformableNodeMap(fragment.getTransformationSystem(), anchorNode, modelRenderable);
-
-        for (int index : transformableNodeMap.keySet()) {
-            initObject(index);
-        }
-
-        updateObjects();
-    }
-
 
     private synchronized void onHostedAnchorAvailable(Anchor anchor) {
-        infoText.setText("onHostedAnchorAvailable was called");
-
-        // called when hosted anchor and respective shortcode is available
-
         Anchor.CloudAnchorState cloudState = anchor.getCloudAnchorState();
         if (cloudState == Anchor.CloudAnchorState.SUCCESS) {
             String cloudAnchorId = anchor.getCloudAnchorId();
             firebaseManager.nextShortCode(shortCode -> {
                 if (shortCode != null) {
-                    setShortCode(shortCode);
                     sceneData.setCloudAnchorId(cloudAnchorId);
                     firebaseManager.storeUsingShortCode(shortCode, sceneData);
-                    infoText.setText("Cloud Anchor Hosted. Short code: " + shortCode);
+                    setMessage("Saving with " + shortCode);
+
+
+                    firebaseManager.getCloudAnchorId(shortCode, cloudAnchorIdLambda -> {
+                        // if anchor ID not found...
+                        if (cloudAnchorIdLambda == null || cloudAnchorIdLambda.isEmpty()) {
+                            setMessage("A Cloud Anchor ID for the short code " + shortCode + " was not found.");
+                            return;
+                        }
+
+                        // resolve cloud anchor
+                        cloudAnchorManager.resolveCloudAnchor(
+                                fragment.getArSceneView().getSession(),
+                                cloudAnchorIdLambda,
+                                anchorLambda -> onResolvedAnchorAvailable(anchorLambda, shortCode));
+                    });
+
+
+
+
+
                 } else {
                     // Firebase could not provide a short code.
-                    infoText.setText("Cloud Anchor Hosted, but could not get a short code from Firebase.");
+                    setMessage("Cloud Anchor Hosted, but could not get a short code from Firebase.");
                 }
             });
         } else {
-            infoText.setText("Error while hosting: " + cloudState.toString());
+            setMessage("Error while hosting: " + cloudState.toString());
         }
     }
 
     private synchronized void onShortCodeEntered(int shortCode) {
-
-        // clear pending listeners
-        cloudAnchorManager.clearListeners();
-
-        // Clear the anchor from the scene.
-        updateAnchor(null);
+        clear();
 
         this.shortCode = shortCode;
-
-        infoText.setText("onShortCodeEntered was called");
 
         firebaseManager.getCloudAnchorId(shortCode, cloudAnchorId -> {
             // if anchor ID not found...
             if (cloudAnchorId == null || cloudAnchorId.isEmpty()) {
-                infoText.setText("A Cloud Anchor ID for the short code " + shortCode + " was not found.");
+                setMessage("A Cloud Anchor ID for the short code " + shortCode + " was not found.");
                 return;
             }
 
@@ -227,115 +310,103 @@ public class MainActivity extends AppCompatActivity {
             cloudAnchorManager.resolveCloudAnchor(
                     fragment.getArSceneView().getSession(),
                     cloudAnchorId,
-                    anchor -> onResolvedAnchorAvailable(anchor, shortCode));
+                    anchorLambda -> onResolvedAnchorAvailable(anchorLambda, shortCode));
         });
     }
 
     private synchronized void onResolvedAnchorAvailable(Anchor anchor, int shortCode) {
+        clear();
+
         this.shortCode = shortCode;
 
-        Anchor.CloudAnchorState cloudState = anchor.getCloudAnchorState();
+        this.anchor = anchor;
+
+        if (redAnchorNode != null) redAnchorNode.setParent(null);
+
+        //set green ball on top of anchor
+        greenAnchorNode = new AnchorNode(this.anchor);
+        greenAnchorNode.setParent(fragment.getArSceneView().getScene());
+        Node node = new Node();
+        node.setParent(greenAnchorNode);
+        node.setRenderable(anchorGreenRenderable);
+
+        Anchor.CloudAnchorState cloudState = this.anchor.getCloudAnchorState();
         if (cloudState == Anchor.CloudAnchorState.SUCCESS) {
-            infoText.setText("Cloud Anchor Resolved. Short code: " + shortCode);
-            updateAnchor(anchor);
+            setMessage("Resolved. Short code: " + shortCode);
+
+            // sceneData has been set according to newly resolved anchor
+
+            transformableNodeMap = sceneData.getTransformableNodeMap(fragment.getTransformationSystem());
+
+            for (int index : transformableNodeMap.keySet()) {
+                initObject(index);
+            }
+
+            updateObjects();
+
         } else {
-            infoText.setText("Error while resolving anchor with short code "+shortCode+". Error: " + cloudState.toString());
+            setMessage("Error while resolving anchor with short code "+shortCode+". Error: " + cloudState.toString());
         }
     }
 
-    public void setSceneData(SceneData sceneData){
-        this.sceneData = sceneData;
-        updateObjects();
-    }
 
-    public void initObject(int index){
-        // put Andy on the anchor
-        TransformableNode node = transformableNodeMap.get(index);
-        NodeData nodeData = sceneData.getNodeData(index);
 
-        node.setParent(anchorNode);
-        node.setRenderable(modelRenderable);
-        node.select();
 
-        node.getScaleController().setMinScale(0.2f);
-        node.getScaleController().setMaxScale(5f);
 
-        /*node.setOnTapListener((hitTestResult, motionEvent) -> {
-            nodeData.setPosition(node.getWorldPosition());
-            nodeData.setScale(node.getWorldScale());
-            nodeData.setRotation(node.getWorldRotation());
-            sceneData.setNodeData(index, nodeData);
-        });
 
-         */
 
-        Log.e("UPDATE", "in initObject: "+shortCode);
 
-        node.setOnTouchListener((hitTestResult, event) -> {
-            Log.e("UPDATE", "in listener: "+shortCode);
 
-            nodeData.setPosition(node.getLocalPosition());
-            nodeData.setScale(node.getLocalScale());
-            nodeData.setRotation(node.getLocalRotation());
-            sceneData.setNodeData(index, nodeData);
 
-            if (event.getAction() == MotionEvent.ACTION_UP ) {
-                if (anchorNode != null) {
-                    firebaseManager.updateSceneData(getShortCode(), sceneData);
-                }
+
+
+    /*
+    HELPER FUNCTIONS
+     */
+
+
+
+
+
+
+    public void clear(){
+        setMessage("cleared");
+
+        // clear pending listeners
+        cloudAnchorManager.clearListeners();
+        // THROWS CONCURRENT EXCEPTION?!?!
+
+        shortCode = 0;
+
+        if (transformableNodeMap != null){
+            for( TransformableNode node : transformableNodeMap.values()){
+                node.getParent().setParent(null); // remove all nodes from the scene
             }
-            return true;
-        });
-    }
-
-    public void updateObjects() {
-
-        for (int index : transformableNodeMap.keySet()) {
-            Log.e("INDEX", ((Integer)index).toString());
-            NodeData nodeData = sceneData.getNodeData(index);
-            TransformableNode node = transformableNodeMap.get(index);
-
-            node.setLocalPosition(nodeData.getPosition());
-            node.setLocalScale(nodeData.getScale());
-            node.setLocalRotation(nodeData.getRotation());
-        }
-    }
-
-/*
-    protected static ObjectAnimator createAnimator(boolean clockwise, float axisTiltDeg) {
-        // Node's setLocalRotation method accepts Quaternions as parameters.
-        // First, set up orientations that will animate a circle.
-        Quaternion[] orientations = new Quaternion[4];
-        // Rotation to apply first, to tilt its axis.
-        Quaternion baseOrientation = Quaternion.axisAngle(new Vector3(1.0f, 0f, 0.0f), axisTiltDeg);
-        for (int i = 0; i < orientations.length; i++) {
-            float angle = i * 360 / (float)(orientations.length - 1);
-            if (clockwise) {
-                angle = 360 - angle;
-            }
-            Quaternion orientation = Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), angle);
-            orientations[i] = Quaternion.multiply(baseOrientation, orientation);
         }
 
-        ObjectAnimator orbitAnimation = new ObjectAnimator();
-        // Cast to Object[] to make sure the varargs overload is called.
-        orbitAnimation.setObjectValues((Object[]) orientations);
+        // throw away the map
+        transformableNodeMap = new HashMap<>();
 
-        // Next, give it the localRotation property.
-        orbitAnimation.setPropertyName("localRotation");
+        if (greenAnchorNode != null) greenAnchorNode.setParent(null);
 
-        // Use Sceneform's QuaternionEvaluator.
-        orbitAnimation.setEvaluator(new QuaternionEvaluator());
-
-        //  Allow orbitAnimation to repeat forever
-        orbitAnimation.setRepeatCount(ObjectAnimator.INFINITE);
-        orbitAnimation.setRepeatMode(ObjectAnimator.RESTART);
-        orbitAnimation.setInterpolator(new LinearInterpolator());
-        orbitAnimation.setAutoCancel(true);
-
-        return orbitAnimation;
+        anchor = null;
     }
 
- */
+    public void setMessage(String message) {
+        infoText.setText(message);
+    }
+
+    public void updateMessages(TransformableNode node) {
+        setMessage("node loc pos: "+node.getLocalPosition().x+" "+node.getLocalPosition().y+" "+node.getLocalPosition().z);
+        setMessage("anchor node pos: "+node.getParent().getLocalPosition().x+" "+node.getParent().getLocalPosition().y+" "+node.getParent().getLocalPosition().z);
+    }
+
+    public int getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(int shortCode) {
+        this.shortCode = shortCode;
+    }
 }
 
